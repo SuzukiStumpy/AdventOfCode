f39a12ddab5e9173e89b5f7605e0241e7be473d3
Mark Edwards <github@medwards-online.co.uk>
2021-12-01 17:20:21 +0000

    Update day1_2.js

--- 2021/day1_2.js
+++ 2021/day1_2.js
@@ -1,5 +1,4 @@
 "use strict";
-debugger;
 
 let input = document.body.textContent.split('\n').map( x => parseInt(x) );
 input.pop();  // Remove the last element (it's not a number)!
@@ -9,7 +8,6 @@ let sums = input.map( (elt, idx, ary) => {
 	}
 });
 
-//Test input   let input = [199,200,208,210,200,207,240,269,260,263];
 let depthCount = 0;
 sums.reduce( (prev, cur) => { if (cur > prev) depthCount++; return cur; } );
 console.log( `Depth increased: ${depthCount} times.` );

b7e71ca3fb15af3f307ada0f8f28eccb1410c586
Mark Edwards <github@medwards-online.co.uk>
2021-12-07 07:02:53 +0000

    Add files via upload

---
+++ 2021/day7_1.js
@@ -0,0 +1,20 @@
+"use strict";
+debugger;
+
+{
+	let input = document.body.textContent
+		.split(',')
+		.filter( x => x !== '' )
+		.map( x => parseInt(x) );
+		
+	let maxPosition = Math.max(...input);
+	let moveList = new Array(maxPosition).fill(0);
+	
+	for (let i=0; i < maxPosition; i++) {
+		for (let j=0; j < input.length; j++) {
+			moveList[i] += Math.abs(input[j] - i);
+		}
+	}
+	
+	console.log(`The position which uses the least amount of fuel is ${Math.min(...moveList)}.`);
+}

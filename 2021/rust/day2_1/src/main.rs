6337f7eadb0dc35378e9a321225e8197bbcb0edc
Mark Edwards <github@medwards-online.co.uk>
2022-01-10 15:44:11 +0000

    Add files via upload

---
+++ 2021/rust/day2_1/src/main.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn load_from_file(file_path :&String) -> Vec<String> {
+    let f = File::open(file_path).expect("File not found");
+    let r = BufReader::new(f);
+
+    r.lines()
+        .map(|line| line.unwrap() )
+        .collect()
+}
+
+struct SubPos {
+    horiz: i32,
+    depth: i32,
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let fname: &String = &args[1];
+
+    let mut pos = SubPos {
+        horiz: 0,
+        depth: 0
+    };
+
+    let lines = load_from_file(&fname);
+
+    for line in lines {
+        let mut l2 = line.split(' ');
+        let command = l2.next().unwrap();
+        let step = l2.next().unwrap().parse::<i32>().unwrap();
+
+        match command {
+            "forward" => pos.horiz += step,
+            "down" => pos.depth += step,
+            "up" => pos.depth -= step,
+            _ => (),
+        }
+    }
+
+    println!("The final sub position is:\n\tHorizontal: {}\n\tDepth: {}", pos.horiz, pos.depth);
+    println!("The final answer required is: {}", (pos.horiz * pos.depth));
+}
